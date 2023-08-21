0d963af29c918c51dd53d95a2a80bff9dd0ab3bb
soyoon1 <20210841@duksung.ac.kr>
2023-08-21 14:34:29 +0900

    프로젝트 생성

---
+++ src/main/java/com/dukbab/SirenOrderApplication.java
@@ -0,0 +1,13 @@
+package com.dukbab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SirenOrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SirenOrderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dukbab/SirenOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dukbab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SirenOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
